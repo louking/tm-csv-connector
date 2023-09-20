0558ec78efe0c69c494945d9f3078047b86a1dea
Lou King <lking@pobox.com>
2023-09-20 09:19:22 -0400

    build tm-csv-connector app (#31)

--- app/src/tm_csv_connector/version.py
+++ app/src/tm_csv_connector/version.py
@@ -1,5 +1,5 @@
 # See https://packaging.python.org/guides/distributing-packages-using-setuptools/#semantic-versioning-preferred
-__version__ = '0.0.1.dev1'
+__version__ = '1.0.0.dev1'
 __docversion__ = __version__
 # uncomment for development
 __docversion__ = 'latest'
