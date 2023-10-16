4ebd20c1a376d7167c7561e1f9eea7f0b8781417
Lou King <lking@pobox.com>
2023-10-16 11:59:48 -0400

    install prev output and logging paths are maintained (#37)

--- install/edit-env.ps1
+++ install/edit-env.ps1
@@ -3,30 +3,48 @@ Install-PackageProvider NuGet -ForceBootstrap
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module -Name PsIni
 
+function Set-Path ($path, $prompt) {
+    if (-not $path) {
+        do {
+            $path = Read-Host $prompt
+        }
+        while ((-not $path) -or (-not (Test-Path $path -IsValid)))
+    }
+    if (-not (Test-Path $path)) {
+        New-Item -Path $path -ItemType Directory | Out-Null
+        Write-Host "'$path' created"
+    }
+    
+    return $path
+}
+
 # https://stackoverflow.com/a/22804178/799921
 $env = Get-IniContent .env
+
+# check if we've installed before
+if (Test-Path .lastenv) {
+    $lastenv = Get-IniContent .lastenv
+    $output_dir = $lastenv["_"]["OUTPUT_DIR"]
+    $logging_dir = $lastenv["_"]["LOGGING_DIR"]
+} else {
+    $lastenv = @{"_" = @{}}
+}
 
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/read-host?view=powershell-7.3
 # https://lazyadmin.nl/powershell/powershell-replace/
 
 # OUTPUT_DIR
-$output_dir = Read-Host "Directory for output csv?"
-if (-not (Test-Path $output_dir)) {
-    New-Item -Path $output_dir -ItemType Directory | Out-Null
-    Write-Host "'$output_dir' created"
-}
+$output_dir = Set-Path "$output_dir" "Directory for output csv?"
 $output_dir = $output_dir.Replace('\', '/')
 $env["_"]["OUTPUT_DIR"] = $output_dir
+$lastenv["_"]["OUTPUT_DIR"] = $output_dir
 
 # LOGGING_DIR
-$logging_dir = Read-Host "Directory for logging?"
-if (-not (Test-Path $logging_dir)) {
-    New-Item -Path $logging_dir -ItemType Directory | Out-Null
-    Write-Host "'$logging_dir' created"
-}
+$logging_dir = Set-Path "$logging_dir" "Directory for logging?"
 $logging_dir = $logging_dir.Replace('\', '/')
 $env["_"]["LOGGING_DIR"] = $logging_dir
+$lastenv["_"]["LOGGING_DIR"] = $logging_dir
 
-# save new .env
+# save new .env, .lastenv
 $env | Out-IniFile -FilePath .env -Encoding ASCII -Force
-
+$lastenv | Out-IniFile -FilePath .lastenv -Encoding ASCII -Force
--- install/enable-tm-reader.ps1
+++ install/enable-tm-reader.ps1
@@ -2,6 +2,6 @@ Import-Env
 $arch = (32, 64)[[bool] ${env:ProgramFiles(x86)}]
 & nssm-2.24/win$arch/nssm install TmReader "$pwd/tm-reader/tm-reader" | Out-Null
 & nssm-2.24/win$arch/nssm set TmReader Description "Time Machine Reader" | Out-Null
-& nssm-2.24/win$arch/nssm set TmReader AppStdout "$env:LOGGING_PATH/tm-reader.log" | Out-Null
-& nssm-2.24/win$arch/nssm set TmReader AppStderr "$env:LOGGING_PATH/tm-reader.log" | Out-Null
+& nssm-2.24/win$arch/nssm set TmReader AppStdout "$env:LOGGING_DIR/tm-reader.log" | Out-Null
+& nssm-2.24/win$arch/nssm set TmReader AppStderr "$env:LOGGING_DIR/tm-reader.log" | Out-Null
 & nssm-2.24/win$arch/nssm start TmReader
