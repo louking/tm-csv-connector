e8b85cac000efbf76cb294e33bfd61d6fcb6b4b5
Lou King <lking@pobox.com>
2024-04-24 16:27:17 -0400

    remove dead code from assets.py

--- app/src/tm_csv_connector/assets.py
+++ app/src/tm_csv_connector/assets.py
@@ -43,121 +43,7 @@ cke_ver='26.0.0-members-414' # https://ckeditor.com/ckeditor-5/
 materialize_ver='1.0.0'     # https://materializecss.com/
 pickadate_ver = '3.6.4'     # https://amsul.ca/pickadate.js/
 
-frontend_common_js = Bundle(
-    f'js/jquery-{jq_ver}/jquery-{jq_ver}.js',
-    f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.js',
-
-    f'js/lodash-{lodash_ver}/lodash.js',
-
-    f'js/smartmenus-{sm_ver}/jquery.smartmenus.js',
-
-    # datatables / yadcf
-    f'js/DataTables-{dt_datatables_ver}/js/jquery.dataTables.js',
-    f'js/DataTables-{dt_datatables_ver}/js/dataTables.jqueryui.js',
-    # 'js/yadcf-{ver}/jquery.dataTables.yadcf.js'.format(ver=yadcf_ver),
-
-    f'js/FixedColumns-{dt_fixedcolumns_ver}/js/dataTables.fixedColumns.js',
-    f'js/Responsive-{dt_responsive_ver}/js/dataTables.responsive.js',
-    f'js/Responsive-{dt_responsive_ver}/js/responsive.jqueryui.js',
-    f'js/DateTime-{dt_datetime_ver}/js/dataTables.dateTime.js',
-
-    f'js/Editor-{dt_editor_ver}/js/dataTables.editor.js',
-    f'js/Editor-{dt_editor_ver}/js/editor.jqueryui.js',
-
-    f'js/Select-{dt_select_ver}/js/dataTables.select.js',
-
-    # select2 is required for use by Editor forms 
-    f'js/select2-{s2_ver}/js/select2.full.js',
-    # # the order here is important
-    'js/FieldType-Select2/editor.select2.js',
-
-    # # date time formatting
-    # 'js/moment-{ver}/moment.js'.format(ver=moment_ver),
-
-    # # d3
-    # 'js/d3-{ver}/d3.js'.format(ver=d3_ver),
-    # 'js/d3-tip-{ver}/d3-tip.js'.format(ver=d3_tip_ver),
-
-    # 'frontend/beforedatatables.js',
-    # 'admin/layout.js',  # TODO: smartmenus initialization, should be moved to layout.js
-    'layout.js',
-
-    'utils.js',                             # from loutilities
-
-    'editor.select2.mymethods.js',          # from loutilities
-    'datatables.js',                        # from loutilities
-    'datatables.dataRender.ellipsis.js',    # from loutilities
-    'datatables.dataRender.datetime.js',    # from loutilities
-    'editor.buttons.editrefresh.js',        # from loutilities
-    'editor.buttons.editchildrowrefresh.js',# from loutilities
-    'filters.js',                           # from loutilities
-    'utils.js',                             # from loutilities
-    'user/admin/groups.js',                 # from loutilities
-
-    # 'admin/afterdatatables.js',             # TODO: should move common bits up a level and pieces to frontend/afterdatatables
-
-    filters='jsmin',
-    output='gen/frontendcommon.js',
-)
-
-frontend_common_css = Bundle(
-    f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.css',
-    f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.structure.css',
-    f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.theme.css',
-    f'js/DataTables-{dt_datatables_ver}/css/dataTables.jqueryui.css',
-    f'js/Buttons-{dt_buttons_ver}/css/buttons.jqueryui.css',
-    f'js/FixedColumns-{dt_fixedcolumns_ver}/css/fixedColumns.jqueryui.css',
-    f'js/Responsive-{dt_responsive_ver}/css/responsive.dataTables.css',
-    f'js/Responsive-{dt_responsive_ver}/css/responsive.jqueryui.css',
-    f'js/DateTime-{dt_datetime_ver}/css/dataTables.dateTime.css',
-    f'js/Select-{dt_select_ver}/css/select.jqueryui.css',
-    f'js/select2-{s2_ver}/css/select2.css',
-    # 'js/yadcf-{ver}/jquery.dataTables.yadcf.css'.format(ver=yadcf_ver),
-
-    f'js/fontawesome-{fa_ver}/css/fontawesome.css', 
-    f'js/fontawesome-{fa_ver}/css/solid.css', 
-    f'js/fontawesome-{fa_ver}/css/regular.css', 
-
-    'datatables.css',  # from loutilities
-    'editor.css',  # from loutilities
-    'filters.css',  # from loutilities
-    'branding.css',  # from loutilities
-
-    f'js/smartmenus-{sm_ver}/css/sm-core-css.css',
-    f'js/smartmenus-{sm_ver}/css/sm-blue/sm-blue.css',
-
-    'style.css',
-    # 'admin/style.css',      # TODO: some of this is for smartmenus, should be in style.css
-
-    filters=['cssrewrite', 'cssmin'],
-    output='gen/frontendcommon.css',
-)
-
 asset_bundles = {
-
-    'frontend_js': Bundle(
-        frontend_common_js,
-    ),
-    
-    'frontendmembers_js': Bundle(
-        frontend_common_js,
-    ),
-    
-    'frontendmaterialize_js': Bundle(
-        frontend_common_js,       
-    ),
-
-    'frontend_css': Bundle(
-        frontend_common_css,
-        
-        output='gen/frontend.css',
-        # cssrewrite helps find image files when ASSETS_DEBUG = False
-        filters=['cssrewrite', 'cssmin'],
-        ),
-
-    'frontendmaterialize_css': Bundle(
-        frontend_common_css,
-    ),
 
     'admin_js': Bundle(
         Bundle(f'js/jquery-{jq_ver}/jquery-{jq_ver}.js', filters='jsmin'),
@@ -271,7 +157,15 @@ asset_bundles = {
         output='gen/admin.css',
         # cssrewrite helps find image files when ASSETS_DEBUG = False
         # filters=['cssrewrite', 'cssmin'],
-        )
+        ),
+    
+    'assets_js': Bundle(
+        'admin_js'
+    ),
+    
+    'assets_css': Bundle(
+        'admin_css'
+    )
 }
 
 asset_env = Environment()
