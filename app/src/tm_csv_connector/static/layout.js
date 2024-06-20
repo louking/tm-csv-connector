3affb917e773510ab98d4727182779fa95a1d804
Lou King <lking@pobox.com>
2024-06-20 06:53:29 -0400

    click of Chips menu tab now doesn't navigate (#78)

--- app/src/tm_csv_connector/static/layout.js
+++ app/src/tm_csv_connector/static/layout.js
@@ -6,6 +6,11 @@ $( function() {
 			subMenusSubOffsetY: -8
     });
 
+    // disable click on smartmenus submenu tab
+    $( ".navbar a.has-submenu" ).click(function(e) {
+        e.preventDefault();
+    });
+
     // all navbar links which are not on this site (i.e., don't start with '/') open in new tab
     $( '.navbar a' ).not('[href^="/"]').attr('target', '_blank');
 
