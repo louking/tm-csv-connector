c24ee89fdaf24cf76b0152939011d16858e48933
Lou King <lking@pobox.com>
2026-03-03 14:45:18 -0500

    results: notify user on attempt to confirm row which has bib alert (#128)

--- app/src/tm_csv_connector/static/beforedatatables.js
+++ app/src/tm_csv_connector/static/beforedatatables.js
@@ -3,6 +3,34 @@ $( function () {
     // required for datatables option {serverside:true}
 });
 
+
+/**
+ * check if any results have bib alert, and if so, check with user before sending confirm
+ * 
+ * @returns true if user confirms or if no bib alerts, false if user cancels
+ */
+function confirmConfirm(dt) {
+    // only one row is selected, the bib alerts need to be checked for any rows up to this one
+    var selected_row = dt.row({selected: true}).data();
+
+    var bibalert_rows = dt
+        .rows()
+        .data()
+        .filter(function(row, instance){
+            // only check rows up to the currently selected row, and only if not already confirmed
+            if (row.is_confirmed != undefined && row.is_confirmed != "") return false;  
+            if (row.placepos > selected_row.placepos) return false;
+
+            return row.bibalert != undefined && row.bibalert != "" && row.bibalert != null;
+        });
+        
+    if (bibalert_rows.length > 0) {
+        return confirm(`There are ${bibalert_rows.length} results with bib alerts. Are you sure you want to confirm them?`);
+    } else {
+        return true;
+    }
+}
+
 /**
  * handles the Confirm button on the Results view
  * 
@@ -12,6 +40,11 @@ $( function () {
  * @param {*} config 
  */
 function results_confirm( e, dt, node, config ) {
+    // check if any results have bib alert, and if so, check with user before sending confirm
+    if (!confirmConfirm(dt)) {
+        return;
+    }
+
     // the preSubmit event is used to annotate the currently selected row with "confirm"
     // this is done only once, for the next edit/submit
     editor.one('preSubmit', function(e, data, action) {
