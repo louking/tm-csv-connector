03825ce3bfca3cddb3ad2de58aeea60c57bcf1ad
Lou King <lking@pobox.com>
2026-06-19 15:51:34 -0400

    datatables 2.x support (#138)

--- app/src/tm_csv_connector/assets.py
+++ app/src/tm_csv_connector/assets.py
@@ -6,19 +6,13 @@ assets - javascript and css asset handling
 from flask_assets import Bundle, Environment
 
 # jquery
-jq_ver = '3.6.4'
-jq_ui_ver = '1.13.2'
+jq_ver = '3.7.1'
+jq_ui_ver = '1.14.2'
 jq_validate_ver = '1.19.3'
 
 # dataTables
-dt_buttons_ver = '2.3.6' # also used for colvis and html5
-dt_datatables_ver = '1.13.4'
-dt_editor_ver = '2.3.1'
-dt_datetime_ver = '1.4.1'
-dt_fixedcolumns_ver = '4.2.2'
-dt_responsive_ver = '2.4.1'
-dt_rowreorder_ver = '1.3.3'
-dt_select_ver = '1.6.2'
+dt_datatables_ver = '2.3.8-pkgs-jqui'
+
 jszip_ver = '2.5.0'
 
 # select2
@@ -30,11 +24,12 @@ s2_ver = '4.0.13'
 sm_ver = '1.2.1'
 
 # yadcf
-yadcf_ver = '0.9.4.beta.45+lk-date_custom_func'
+yadcf_ver = '2.0.1.beta.9.louking.3'
+yadcf_suffix = '-2.0'
 
 moment_ver = '2.29.4'       # moment.js (see https://momentjs.com/)
 lodash_ver = '4.17.21'      # lodash.js (see https://lodash.com)
-d3_ver = '7.1.1'            # d3js.org (see https://d3js.org/)
+d3_ver = '7.4.2'
 d3_tip_ver = '1.1'          # https://github.com/VACLab/d3-tip
 fa_ver = '6.5.1'            # https://fontawesome.com/
 nunjucks_ver = '3.2.0'      # https://mozilla.github.io/nunjucks/
@@ -46,7 +41,7 @@ pickadate_ver = '3.6.4'     # https://amsul.ca/pickadate.js/
 asset_bundles = {
 
     'admin_js': Bundle(
-        Bundle(f'js/jquery-{jq_ver}/jquery-{jq_ver}.js', filters='jsmin'),
+        Bundle(f'js/jQuery-{jq_ver}/jquery-{jq_ver}.js', filters='jsmin'),
         Bundle(f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.js', filters='jsmin'),
 
         Bundle(f'js/smartmenus-{sm_ver}/jquery.smartmenus.js', filters='jsmin'),
@@ -55,29 +50,18 @@ asset_bundles = {
         Bundle('beforedatatables.js', filters='jsmin'),
 
         # Bundle('js/JSZip-{ver}/jszip.js'.format(ver=jszip_ver), filters='jsmin'),
-        Bundle(f'js/DataTables-{dt_datatables_ver}/js/jquery.dataTables.js', filters='jsmin'),
-        Bundle(f'js/DataTables-{dt_datatables_ver}/js/dataTables.jqueryui.js', filters='jsmin'),
-        Bundle(f'js/Editor-{dt_editor_ver}/js/dataTables.editor.js', filters='jsmin'),
-        Bundle(f'js/Editor-{dt_editor_ver}/js/editor.jqueryui.js', filters='jsmin'),
-        Bundle(f'js/Buttons-{dt_buttons_ver}/js/dataTables.buttons.js', filters='jsmin'),
-        Bundle(f'js/Buttons-{dt_buttons_ver}/js/buttons.jqueryui.js', filters='jsmin'),
-        Bundle(f'js/Buttons-{dt_buttons_ver}/js/buttons.colVis.js', filters='jsmin'),
-        Bundle(f'js/Buttons-{dt_buttons_ver}/js/buttons.html5.js', filters='jsmin'),
-        Bundle(f'js/FixedColumns-{dt_fixedcolumns_ver}/js/dataTables.fixedColumns.js', filters='jsmin'),
-        Bundle(f'js/Responsive-{dt_responsive_ver}/js/dataTables.responsive.js', filters='jsmin'),
-        Bundle(f'js/RowReorder-{dt_rowreorder_ver}/js/dataTables.rowReorder.js', filters='jsmin'),
-        Bundle(f'js/Select-{dt_select_ver}/js/dataTables.select.js', filters='jsmin'),
-        Bundle(f'js/DateTime-{dt_datetime_ver}/js/dataTables.dateTime.js', filters='jsmin'),
+        Bundle(f'js/DataTables-{dt_datatables_ver}/datatables.js', filters='jsmin'),
+        
         Bundle(f'js/moment-{moment_ver}/js/moment.js', filters='jsmin'),
 
         Bundle('afterdatatables.js', filters='jsmin'),
 
-        Bundle('js/yadcf-{ver}/jquery.dataTables.yadcf.js'.format(ver=yadcf_ver), filters='jsmin'),
+        Bundle(f'js/yadcf-{yadcf_ver}/jquery.dataTables.yadcf{yadcf_suffix}.js', filters='jsmin'),
 
         # # select2 is required for use by Editor forms
         Bundle(f'js/select2-{s2_ver}/js/select2.full.js', filters='jsmin'),
         # # the order here is important
-        Bundle('js/FieldType-Select2/editor.select2.js', filters='jsmin'),
+        Bundle('js/FieldType-Select2/editor.select2-v4.js', filters='jsmin'),
 
         # # date time formatting for datatables editor, per https://editor.datatables.net/reference/field/datetime
         # Bundle('js/moment-{ver}/moment.js'.format(ver=moment_ver), filters='jsmin'),
@@ -127,19 +111,11 @@ asset_bundles = {
         Bundle(f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.css', filters=['cssrewrite', 'cssmin']),
         Bundle(f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.structure.css', filters=['cssrewrite', 'cssmin']),
         Bundle(f'js/jquery-ui-{jq_ui_ver}.custom/jquery-ui.theme.css', filters=['cssrewrite', 'cssmin']),
+        Bundle(f'js/DataTables-{dt_datatables_ver}/datatables.css', filters=['cssrewrite', 'cssmin']),
+        
         Bundle(f'js/smartmenus-{sm_ver}/css/sm-core-css.css', filters=['cssrewrite', 'cssmin']),
         Bundle(f'js/smartmenus-{sm_ver}/css/sm-blue/sm-blue.css', filters=['cssrewrite', 'cssmin']),
 
-        Bundle(f'js/DataTables-{dt_datatables_ver}/css/dataTables.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/Editor-{dt_editor_ver}/css/editor.dataTables.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/Editor-{dt_editor_ver}/css/editor.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/Buttons-{dt_buttons_ver}/css/buttons.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/FixedColumns-{dt_fixedcolumns_ver}/css/fixedColumns.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/Responsive-{dt_responsive_ver}/css/responsive.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/RowReorder-{dt_rowreorder_ver}/css/rowReorder.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/Select-{dt_select_ver}/css/select.jqueryui.css', filters=['cssrewrite', 'cssmin']),
-        Bundle(f'js/DateTime-{dt_datetime_ver}/css/dataTables.dateTime.css', filters=['cssrewrite', 'cssmin']),
-        
         Bundle(f'js/select2-{s2_ver}/css/select2.css', filters=['cssrewrite', 'cssmin']),
         Bundle('js/yadcf-{ver}/jquery.dataTables.yadcf.css'.format(ver=yadcf_ver), filters=['cssrewrite', 'cssmin']),
 
